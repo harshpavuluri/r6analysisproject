04de79e0128e5d0452891c028af882a0054fe604
pavuluriharsh <pavuluriharsh@gmail.com>
2021-01-15 15:13:35 -0500

    Split attacker defender data frames

--- src/main.py
+++ src/main.py
@@ -1,25 +1,33 @@
 import json
 import pandas as pd
 
-with open('input.json') as f:
-  data = json.load(f)
+def main():
+  with open('input.json') as f:
+    data = json.load(f)
 
-# print(data)
-# print(json.dumps(data, indent = 4, sort_keys=False))
+  # print(data)
+  # print(json.dumps(data, indent = 4, sort_keys=False))
 
-# print(data['operators'][1])
+  # print(data['operators'][1])
 
-# print(len(data['operators']))
+  # print(len(data['operators']))
 
-column_names = ["name", "ctu", "role","kills","deaths","kd","wins","losses","wl","headshots","dbnos","melee_kills","experience","playtime"]
+  column_names = ["name", "ctu", "role","kills","deaths","kd","wins","losses","wl","headshots","dbnos","melee_kills","experience","playtime"]
 
-df = pd.DataFrame(columns = column_names)
+  df = pd.DataFrame(columns = column_names)
 
-for i in data['operators']:
-    i.pop('abilities', None)
-    i.pop('badge_image', None)
-    df = df.append(i,ignore_index=True)
+  for i in data['operators']:
+      i.pop('abilities', None)
+      i.pop('badge_image', None)
+      df = df.append(i,ignore_index=True)
 
-print(df)
 
-        
+
+  attacker_df = df[df['role'] == "Attacker"]
+  defender_df = df[df['role'] == "Defender"]
+
+  print(attacker_df)
+  print(defender_df)
+  pass
+
+main()
