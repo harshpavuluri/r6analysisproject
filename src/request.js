cb1b62d741ad63b08a742dc259be5f9002762507
pavuluriharsh <pavuluriharsh@gmail.com>
2021-01-18 14:45:06 -0500

    Getting seasonal stats

--- src/request.js
+++ src/request.js
@@ -9,7 +9,7 @@ const getGenericStats = async ({ username, platform }) => {
   })
 
 //   console.log({ data })
-  console.log(JSON.stringify(data, null, 2))
+ // console.log(JSON.stringify(data, null, 2))
   var fs = require('fs');
   fs.writeFile ("input.json", JSON.stringify(data), function(err) {
     if (err) throw err;
@@ -18,7 +18,29 @@ const getGenericStats = async ({ username, platform }) => {
 );
 }
 
+const getSeasonalStats = async ({ username, platform }) => {
+  const { data } = await axios.get(`https://api2.r6stats.com/public-api/stats/${username}/${platform}/seasonal`, {
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  })
+
+  console.log({ data })
+  console.log(JSON.stringify(data), null, 2)
+  var file = require('fs');
+  file.writeFile ("seasonal.json", JSON.stringify(data), function(err) {
+    if (err) throw err;
+    console.log('complete');
+    }
+);
+}
+
 getGenericStats({
+  username: 'Kuri_NEON',
+  platform: 'pc',
+})
+
+getSeasonalStats({
   username: 'Kuri_NEON',
   platform: 'pc',
 })
