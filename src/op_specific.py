d0a4a56b5fe0e2785fd32f73144c2628c8cffc0c
pavuluriharsh <pavuluriharsh@gmail.com>
2021-01-25 11:02:19 -0500

    finished defense sorting

--- src/op_specific.py
+++ src/op_specific.py
@@ -1,31 +1,5 @@
 from genarics import *
 
-# op roles
-'''
-For attack:
-Hard Breacher- This is the guy who will open the hard wall of the objective. It will often times also be the person who will plant the diffuser. This persons main goal is to survive long enough to open up the objective, and that’s it. It is often times played by players in ranked not having the best of days, as it does not require a great amount of quick thinking or quick flicks to play excellently. Operators in this category include Thermite and Hibana.
-
-Vertical Operators- The job of the vertical players is to force the defenders, specifically anchors out of their typical hiding spots. You want to force the anchors into the lines of sight of your teammates. This is one of the most underrated play-styles at lower levels, and you will almost always see a vertical operator being picked in pro league. These operators include Buck and Sledge.
-
-True Fraggers- The job of fraggers is to get kills. Pretty simple. They are your teammates that are responsible for really pushing the objective and taking out the enemies. Operators in this category include Ash and Zofia.
-
-Anti-Roamers: The job of an anti roamer is to clear the map of all roamers, so your team only has to focus on the objective. The anti-roamer is often responsible for gaining map control of the entire map. They are also a very useful intel operator, as they can pinpoint locations of the lethal roamers. The main anti-roaming operators are Jackal and Lion.
-
-Support Ops- The job of the supporting operators are to simply help get the diffuser down. It is a much broader category, as it can include a much wider variety of operators, but these operators are in it to help the planter, and that’s it. Operators in this category include Capitao, Ying, and Gridlock. Shields could also go in this category.
-
-That is mainly it for the attacking roles, you will often times see one of each of these on a pro league lineup, as attack is far more structured than the defense.
-
-For defense:
-
-Roamer- The job of the roamer is to make the attackers waste time. So many people get this wrong, as the general consensus is that the roamers should look to get kills. This is a horrible mindset to be in as a roamer, as this will cause you to play far more aggressively, which will mean that you will die, making the whole round easier for the attackers. You merely want to force the attackers to pick utility to try to find you, and to make the attackers paranoid to be in certain spots because of the threat of a flank. The best roamers in siege include Jager, Caviera, and Alibi.
-
-Anchor- The job of the anchor is to prevent the plant. You as a roamer should do everything in your power to make sure that the attacking team has as many problems as possible getting the diffuser down. You are also the last line of defense if the attacking team takes out all the players off site. Anchors typically stay in sights, and are the main camera/callout players in the start of a round. The best anchors in Siege are Maestro, Rook, and Doc.
-
-Lurkers- Lurking operators stay close to the objective and try to prevent the attacking team from getting to close to the site. Lurkers like to watch hallways adjacent to the site, or main entrances the attacking team typically likes coming in from. The main lurkers in siege include Lesion and Valkyrie.
-
-Wall Denial- The only job of the wall denial operators is to make sure that the attacking team isn’t able to plant the diffuser in a spot that would make it hard for the defensive team to anti-diffuse. The wall denial operators will deny the hard walls in the objective to limit the ways the attacking team can get in. The best wall denial operators are Mute, Bandit, and Kaid.
-
-'''
 
 def role_stat(df_role, op_list):
     pass
@@ -39,11 +13,11 @@ def attacker_stats(df):
     
     pass
 
-#need to finish sorting through def
+
 def defender_stats(df):
-    roam = ["Jager" , "Caviera" , "Alibi", "Ela"]
-    anchor = ["Maestro", "Rook" , "Doc", "Echo", "Mira", "Castle"]
-    lurkers = ["Lesion" , "Valkyrie", "Mozzie", "Wamai", "Pulse"]
-    wall_den = ["Mute", "Bandit" , "Kaid", "Aruni"]
+    roam = ["Jager" , "Caviera" , "Alibi", "Ela", "Vigil", "Melusi", "Oryx"]
+    anchor = ["Maestro", "Rook" , "Doc", "Echo", "Mira", "Castle", "Smoke", "Tachanka", "Clash"]
+    lurkers = ["Lesion" , "Valkyrie", "Mozzie", "Wamai", "Pulse", "Frost", "Goyo", ""]
+    wall_den = ["Mute", "Bandit" , "Kaid", "Aruni", "Kapkan"]
 
     pass
