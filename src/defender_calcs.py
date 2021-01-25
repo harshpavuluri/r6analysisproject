0f85a15feeef81fd0ef2ae920c343f2cd4d7ccc7
pavuluriharsh <pavuluriharsh@gmail.com>
2021-01-25 10:40:16 -0500

    class retructure for faster run + op roles

--- src/defender_calcs.py
+++
@@ -1,3 +0,0 @@
-from json_format_op import defender_df
-from genarics import *
-
--- src/runner.py
+++ src/runner.py
@@ -1,7 +1,6 @@
 from json_format_op import defender_df
 from json_format_op import attacker_df
 from json_format_op import all_ops_df
-from three_speed import runner
 
 def main():
     runner()
